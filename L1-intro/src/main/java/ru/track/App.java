7bf2483530128fa52bce34efc37eb60850f73893
MikhailCh <mike_ch@mail.ru>
2018-02-27 18:27:10 +0300

    intro

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -1,5 +1,9 @@
 package ru.track;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
 /**
  * TASK:
  * POST request to  https://guarded-mesa-31536.herokuapp.com/track
@@ -17,13 +21,14 @@ public class App {
     public static final String FIELD_EMAIL = "email";
 
     public static void main(String[] args) throws Exception {
-       // 1) Use Unirest.post()
-        // 2) Get response .asJson()
-        // 3) Get json body and JsonObject
-        // 4) Get field "success" from JsonObject
+        HttpResponse<JsonNode> response = Unirest.post(URL)
+                .header("accept", "application/json")
+                .field(FIELD_NAME, "Churyakov Mikhail")
+                .field(FIELD_GITHUB, "https://github.com/mikhailchuryakov")
+                .field(FIELD_EMAIL, "mike_ch@mail.ru")
+                .asJson();
 
-
-
+        response.getBody().getObject().get("success");
 
         boolean success = false;
     }
