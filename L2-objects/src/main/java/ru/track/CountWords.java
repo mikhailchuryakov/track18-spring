aa7a4f62605fb7dffbb362710e2f746729dba0f1
MikhailCh <mike_ch@mail.ru>
2018-02-28 18:26:06 +0300

    countwords

--- L2-objects/src/main/java/ru/track/CountWords.java
+++ L2-objects/src/main/java/ru/track/CountWords.java
@@ -1,6 +1,9 @@
 package ru.track;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 /**
@@ -40,7 +43,24 @@ public class CountWords {
      * @return - целое число - сумма всех чисел из файла
      */
     public long countNumbers(File file) throws Exception {
-        return 0;
+        long result = 0;
+        try (
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr)
+        ) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                try {
+                    result += Integer.parseInt(line);
+                } catch (NumberFormatException e) {
+                    //
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("error message");
+            throw e;
+        }
+        return result;
     }
 
 
@@ -52,8 +72,32 @@ public class CountWords {
      * @return - результирующая строка
      */
     public String concatWords(File file) throws Exception {
-        return null;
+        StringBuilder str = new StringBuilder("");
+        try (
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr)
+        ) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!line.equals(skipWord)) {
+                    if (!checkNumber(line)) str.append(line).append(" ");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error message");
+            throw e;
+        }
+        return str.toString();
     }
 
+    private boolean checkNumber(String line){
+        char[] array = line.toCharArray();
+        int i = 0;
+        for (char arr: array){
+            if (arr < '0' || arr > '9')
+                return false;
+        }
+        return true;
+    }
 }
 
