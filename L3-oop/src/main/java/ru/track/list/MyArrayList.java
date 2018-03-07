e1abb9d3972e291903e3a2c80e421b3931991370
MikhailCh <mike_ch@mail.ru>
2018-03-07 23:30:27 +0300

    Linked and Array Lists

--- L3-oop/src/main/java/ru/track/list/MyArrayList.java
+++ L3-oop/src/main/java/ru/track/list/MyArrayList.java
@@ -1,5 +1,6 @@
 package ru.track.list;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 /**
@@ -10,32 +11,43 @@ import java.util.NoSuchElementException;
  * - с аргументом - начальный размер массива
  */
 public class MyArrayList extends List {
+    private int[] array;
 
     public MyArrayList() {
-
+        array = new int[0];
     }
 
     public MyArrayList(int capacity) {
-
+        array = new int[capacity];
     }
 
     @Override
     void add(int item) {
-
+        int[] arr = new int[array.length + 1];
+        System.arraycopy(array, 0, arr, 0, array.length);
+        arr[array.length] = item;
+        array = arr;
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx > array.length - 1) throw new NoSuchElementException();
+        int[] arr = new int[array.length - 1];
+        int item = array[idx];
+        System.arraycopy(array, 0, arr, 0, idx);
+        System.arraycopy(array, idx + 1, arr, idx, array.length - 1 - idx);
+        array = arr;
+        return item;
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if (idx > array.length - 1) throw new NoSuchElementException();
+        return array[idx];
     }
 
     @Override
     int size() {
-        return 0;
+        return array.length;
     }
 }
--- L3-oop/src/main/java/ru/track/list/MyLinkedList.java
+++ L3-oop/src/main/java/ru/track/list/MyLinkedList.java
@@ -26,22 +26,57 @@ public class MyLinkedList extends List {
         }
     }
 
+    private static Node head;
+    private int count = 0;
+
     @Override
     void add(int item) {
+        count++;
+        if (head == null) {
+            head = new Node(null, null, item);
+            return;
+        }
+        Node node = head;
+        while (node.next != null){
+            node = node.next;
+        }
+        node.next = new Node (node, null, item);
     }
 
     @Override
     int remove(int idx) throws NoSuchElementException {
-        return 0;
+        int item;
+        if (head == null) throw new NoSuchElementException();
+        Node node = head;
+        if (head.next == null && idx == 0){
+            item = head.val;
+            head = null;
+            count--;
+            return item;
+        }
+        for (int i = 1; i < idx; i++){
+            if (node.next == null) throw new NoSuchElementException();
+            node = node.next;
+        }
+        count--;
+        item = node.val;
+        node.next = node.next.next;
+        return item;
     }
 
     @Override
     int get(int idx) throws NoSuchElementException {
-        return 0;
+        if (head == null) throw new NoSuchElementException();
+        Node node = head;
+        for (int i = 0; i < idx; i++){
+            if (node == null) throw new NoSuchElementException();
+            node = node.next;
+        }
+        return node.val;
     }
 
     @Override
     int size() {
-        return 0;
+        return count;
     }
 }
